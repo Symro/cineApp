e34cba2e912d3c7b34c40d9030edb6334bb1716d
Symro <sylvain.mro@gmail.com>
2014-06-05 01:47:41 +0200

    ajout données dynamiques a la seconde vue

--- cineapp/src/main/java/cineapp/app/MovieDetails.java
+++ cineapp/src/main/java/cineapp/app/MovieDetails.java
@@ -2,8 +2,13 @@ package cineapp.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Sylvain on 02/06/2014.
@@ -11,11 +16,51 @@ import android.view.MenuItem;
  */
 public class MovieDetails extends Activity {
 
+    private ImageView ivPosterImage;
+    private TextView tvTitle;
+    private TextView tvSynopsis;
+    private TextView tvCast;
+    private TextView tvAudienceScore;
+    private TextView tvCriticsScore;
+    private TextView tvCriticsConsensus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        // On récupère les éléments de notre vue
+        ivPosterImage = (ImageView) findViewById(R.id.ivPosterImage);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvSynopsis = (TextView) findViewById(R.id.tvSynopsis);
+        tvCast = (TextView) findViewById(R.id.tvCast);
+        tvCriticsConsensus = (TextView) findViewById(R.id.tvCriticsConsensus);
+        tvAudienceScore =  (TextView) findViewById(R.id.tvAudienceScore);
+        tvCriticsScore = (TextView) findViewById(R.id.tvCriticsScore);
+
+        // On utilise notre model pour peupler notre vue des bonnes données
+        MovieData movie = (MovieData)
+                getIntent().getSerializableExtra(MovieActivity.MOVIE_DETAIL_KEY);
+
+        // on appelle la methode qui va permettre d'afficher ces données dans la vue
+        loadMovie(movie);
+
+    }
+
+    public void loadMovie(MovieData movie) {
+
+        // On envoie les données à la vue
+        tvTitle.setText(movie.getTitle());
+        tvCriticsScore.setText(Html.fromHtml("<b>Critics Score:</b> " + movie.getCriticsScore() + "%"));
+        tvAudienceScore.setText(Html.fromHtml("<b>Audience Score:</b> " + movie.getAudienceScore() + "%"));
+        tvCast.setText(movie.getCastList());
+        tvSynopsis.setText(Html.fromHtml("<b>Synopsis:</b> " + movie.getSynopsis()));
+        tvCriticsConsensus.setText(Html.fromHtml("<b>Consensus:</b> " + movie.getCriticsConsensus()));
+
+        Picasso.with(this).load(movie.getLargePosterUrl()).
+                placeholder(R.drawable.large_movie_poster).
+                into(ivPosterImage);
 
     }
 
@@ -36,7 +81,7 @@ public class MovieDetails extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        
+
         if (id == R.id.action_settings) {
             return true;
         }
